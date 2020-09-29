b3d061ca354f01fb50870e4c2824dcbc0f482eb3
vian2298 <vianey2298@ciencias.unam.mx>
2020-09-29 17:10:05 -0500

    update

--- fluid simulation/fluid.js
+++ fluid simulation/fluid.js
@@ -1,99 +1,105 @@
-let N = 250;
+let N = 256;
 let iter = 16;
 let SCALE = 4;
 let t = 0;
 
-// function to use 1D array and fake the extra two dimensions --> 2D
+// function to use 1D array and fake the extra two dimensions --> 3D
 function IX(x, y) {
   return x + y * N;
 }
 
-
-
+// Fluid cube class
 class Fluid {
-
   constructor(dt, diffusion, viscosity) {
-
     this.size = N;
     this.dt = dt;
     this.diff = diffusion;
     this.visc = viscosity;
 
-    // Intializes array of boolean values to determine if something is a wall or not [1 if wall 0 otherwise]
-
-    this.wall= new Array(N*N);
-
-    for (var y=0; y<this.size; y++) {
-        for (var x=0; x<this.size; x++) {
-            walll[x+y*xdim] = false;
-        }
-    }
-
-    // Array to represent the distribution of densities along the 2D array
-
-    this.density = new Array(N * N).fill(0);
     this.s = new Array(N * N).fill(0);
-
-    // Array to represent the distribution of velocites along the x-axis and y-axis
+    this.density = new Array(N * N).fill(0);
 
     this.Vx = new Array(N * N).fill(0);
     this.Vy = new Array(N * N).fill(0);
 
-    // Arrays to represent the distribution of velocites along the x-axis and y-axis in the previous state
-
     this.Vx0 = new Array(N * N).fill(0);
     this.Vy0 = new Array(N * N).fill(0);
+  }
+  
 
+  // step method
+  step() {
+    let N = this.size;
+    let visc = this.visc;
+    let diff = this.diff;
+    let dt = this.dt;
+    let Vx = this.Vx;
+    let Vy = this.Vy;
+    let Vx0 = this.Vx0;
+    let Vy0 = this.Vy0;
+    let s = this.s;
+    let density = this.density;
 
+    diffuse(1, Vx0, Vx, visc, dt);
+    diffuse(2, Vy0, Vy, visc, dt);
 
+    project(Vx0, Vy0, Vx, Vy);
 
-      // Move particles along their directions of motion:
-    function stream() {
-        barrierCount = 0; barrierxSum = 0; barrierySum = 0;
-        barrierFx = 0.0; barrierFy = 0.0;
-        for (var y=ydim-2; y>0; y--) {          // first start in NW corner...
-            for (var x=1; x<xdim-1; x++) {
-                nN[x+y*xdim] = nN[x+(y-1)*xdim];            // move the north-moving particles
-                nNW[x+y*xdim] = nNW[x+1+(y-1)*xdim];        // and the northwest-moving particles
-            }
-        }
-        for (var y=ydim-2; y>0; y--) {          // now start in NE corner...
-            for (var x=xdim-2; x>0; x--) {
-                nE[x+y*xdim] = nE[x-1+y*xdim];          // move the east-moving particles
-                nNE[x+y*xdim] = nNE[x-1+(y-1)*xdim];        // and the northeast-moving particles
-            }
-        }
-        for (var y=1; y<ydim-1; y++) {          // now start in SE corner...
-            for (var x=xdim-2; x>0; x--) {
-                nS[x+y*xdim] = nS[x+(y+1)*xdim];            // move the south-moving particles
-                nSE[x+y*xdim] = nSE[x-1+(y+1)*xdim];        // and the southeast-moving particles
-            }
-        }
-        for (var y=1; y<ydim-1; y++) {              // now start in the SW corner...
-            for (var x=1; x<xdim-1; x++) {
-                nW[x+y*xdim] = nW[x+1+y*xdim];          // move the west-moving particles
-                nSW[x+y*xdim] = nSW[x+1+(y+1)*xdim];        // and the southwest-moving particles
-            }
-        }
-        for (var y=1; y<ydim-1; y++) {              // Now handle bounce-back from barriers
-            for (var x=1; x<xdim-1; x++) {
-                if (barrier[x+y*xdim]) {
-                    var index = x + y*xdim;
-                    nE[x+1+y*xdim] = nW[index];
-                    nW[x-1+y*xdim] = nE[index];
-                    nN[x+(y+1)*xdim] = nS[index];
-                    nS[x+(y-1)*xdim] = nN[index];
-                    nNE[x+1+(y+1)*xdim] = nSW[index];
-                    nNW[x-1+(y+1)*xdim] = nSE[index];
-                    nSE[x+1+(y-1)*xdim] = nNW[index];
-                    nSW[x-1+(y-1)*xdim] = nNE[index];
-                    // Keep track of stuff needed to plot force vector:
-                    barrierCount++;
-                    barrierxSum += x;
-                    barrierySum += y;
-                    barrierFx += nE[index] + nNE[index] + nSE[index] - nW[index] - nNW[index] - nSW[index];
-                    barrierFy += nN[index] + nNE[index] + nNW[index] - nS[index] - nSE[index] - nSW[index];
-                }
-            }
-        }
+    advect(1, Vx, Vx0, Vx0, Vy0, dt);
+    advect(2, Vy, Vy0, Vx0, Vy0, dt);
 
+    project(Vx, Vy, Vx0, Vy0);
+    diffuse(0, s, density, diff, dt);
+    advect(0, density, s, Vx, Vy, dt);
+  }
+
+  // method to add density
+  addDensity(x, y, amount) {
+    let index = IX(x, y);
+    this.density[index] += amount;
+  }
+
+  // method to add velocity
+  addVelocity(x, y, amountX, amountY) {
+    let index = IX(x, y);
+    this.Vx[index] += amountX;
+    this.Vy[index] += amountY;
+  }
+
+  // function to render density
+  renderD() {
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < N; j++) {
+        let x = i * SCALE;
+        let y = j * SCALE;
+        let d = this.density[IX(i, j)];
+        fill(d);
+        noStroke();
+        rect(x, y, SCALE, SCALE);
+      }
+    }
+  }
+
+  // function to render velocity
+  renderV() {
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < N; j++) {
+        let x = i * SCALE;
+        let y = j * SCALE;
+        let vx = this.Vx[IX(i, j, k)];
+        let vy = this.Vy[IX(i, j, k)];
+
+        if (!(abs(vx) < 0.1 && abs(vy) <= 0.1)) {
+          line(x, y, x + vx * SCALE, y + vy * SCALE);
+        }
+      }
+    }
+  }
+
+  // add barrier
+  barrier(){
+    fill('red');
+  ellipse(mouseX, mouseY, 80, 80);
+	
+  }
+}
--- fluid simulation/sketch.js
+++ fluid simulation/sketch.js
@@ -3,106 +3,97 @@ let barrier = new Array(xdim*ydim);
 let button;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(588, 380);
   /*button = createButton('Add Barrier');*/
   frameRate(50);
+  
   fluid = new Fluid(0.2, 0, 0.0000001);
 }
 
-x = [300,450,600,750]; // X position of each chair
-y = [100,220,340,460]; // Y position of each chair
-
-function draw() {
+function draw(){
+    stroke(51);
+    strokeWeight(2);
   
-  stroke(51);
-  strokeWeight(2);
-
-  let cx = int((0.1 * width) / SCALE);
-  let cy = int((0.1 * height) / SCALE);
-  let cx1 = int((0.5 * width) / SCALE);
-  let cy1 = int((0.5 * height) / SCALE);
-  for (let i = -1; i <= 1; i++) {
-    for (let j = -1; j <= 1; j++) {
-      fluid.addDensity(cx + i, cy + j, random(50, 150));
-      fluid.addDensity(cx1 + i, cy1 + j, random(50, 150));
+    let ax = 60
+    let ay = 90
+    let bx = 75
+    let by = 90
+    let cx = 90
+    let cy = 90
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        fluid.addDensity(ax + i, ay + j, random(50, 150));
+        fluid.addDensity(bx + i, by + j, random(50, 150));
+        fluid.addDensity(cx + i, cy + j, random(50, 150));
+      }
     }
-  }
-
-  for (let i = 0; i < 2; i++) {
-    let angle = noise(t) * TWO_PI * 2;
-    let v = p5.Vector.fromAngle(angle);
-    v.mult(0.2);
-    t += 0.01;
-    fluid.addVelocity(cx, cy, v.x, v.y);
-    fluid.addVelocity(cx1, cy1, -v.x, -v.y);
-  }
-
-
-  fluid.step();
-  fluid.renderD();
-}
-var  students = function(studentsPostionX, studentsPositionY){
-    noFill();
-    ellipse(studentsPostionX, studentsPositionY, 80, 80);
+  
+    for (let i = 0; i < 2; i++) { 
+      let angle = -80;
+      let v = p5.Vector.fromAngle(angle);
+      v.mult(0.2);
+      let angle1 = 45 ;
+      let v1 = p5.Vector.fromAngle(angle1);
+      v1.mult(0.2);
+      t += 0.01;
+      fluid.addVelocity(ax, ay, -v1.x, -v1.y);
+      fluid.addVelocity(bx, by, v.x, -v.y);
+      fluid.addVelocity(cx, cy, v1.x, -v1.y);
+        
     }
+  
+  
+    fluid.step();
+    fluid.renderD();
+ }
 
-function canvasToGrid(canvasX, canvasY) {
-    var gridX = Math.floor(canvasX / pxPerSquare);
-    var gridY = Math.floor((canvas.height - 1 - canvasY) / pxPerSquare); 	// off by 1?
-    return { x:gridX, y:gridY };
-}
+ /*function draw(){
+    stroke(51);
+    strokeWeight(2);
+  
+    let ax = 15
+    let ay = 10
+    let bx = 40
+    let by = 40
+    let cx = 100
+    let cy = 40
+    let dx = 40
+    let dy = 75
+    let fx = 70
+    let fy = 75
+    let ex = 100
+    let ey = 75
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        fluid.addDensity(ax + i, ay + j, random(50, 150));
+        fluid.addDensity(bx + i, by + j, random(50, 150));
+        fluid.addDensity(cx + i, cy + j, random(50, 150));
+        fluid.addDensity(dx + i, dy + j, random(50, 150));
+        fluid.addDensity(fx + i, fy + j, random(50, 150));
+        fluid.addDensity(ex + i, ey + j, random(50, 150));
+      }
+    }
+  
+    for (let i = 0; i < 2; i++) { 
+      let angle = noise(t)*TWO_PI*2 ;
+      let v = p5.Vector.fromAngle(angle);
+      v.mult(0.2);
+      t += 0.01;
+      fluid.addVelocity(ax, ay, v.x, v.y);
+      fluid.addVelocity(bx, by, v.x, v.y);
+      fluid.addVelocity(cx, cy, v.x, v.y);
+      fluid.addVelocity(dx, dy, v.x, v.y);
+      fluid.addVelocity(fx, fy, v.x, v.y);
+      fluid.addVelocity(ex, ey, v.x, v.y);
+        
+    }
+  
+  
+    fluid.step();
+    fluid.renderD();
+ }*/
 
-// Add a barrier at a given grid coordinate location:
-function addBarrier(x, y) {
-    if ((x > 1) && (x < xdim-2) && (y > 1) && (y < ydim-2)) {
-        barrier[x+y*xdim] = true;
-    }
-}
 
-// Remove a barrier at a given grid coordinate location:
-function removeBarrier(x, y) {
-    if (barrier[x+y*xdim]) {
-        barrier[x+y*xdim] = false;
-        paintCanvas();
-    }
-}
 
-// Clear all barriers:
-function clearBarriers() {
-    for (var x=0; x<xdim; x++) {
-        for (var y=0; y<ydim; y++) {
-            barrier[x+y*xdim] = false;
-        }
-    }
-    paintCanvas();
-}
-function placePresetBarrier() {
-    var index = barrierSelect.selectedIndex;
-    if (index == 0) return;
-    clearBarriers();
-    var bCount = barrierList[index-1].locations.length/2;	// number of barrier sites
-    // To decide where to place it, find minimum x and min/max y:
-    var xMin = barrierList[index-1].locations[0];
-    var yMin = barrierList[index-1].locations[1];
-    var yMax = yMin;
-    for (var siteIndex=2; siteIndex<2*bCount; siteIndex+=2) {
-        if (barrierList[index-1].locations[siteIndex] < xMin) {
-            xMin = barrierList[index-1].locations[siteIndex];
-        }
-        if (barrierList[index-1].locations[siteIndex+1] < yMin) {
-            yMin = barrierList[index-1].locations[siteIndex+1];
-        }
-        if (barrierList[index-1].locations[siteIndex+1] > yMax) {
-            yMax = barrierList[index-1].locations[siteIndex+1];
-        }
-    }
-    var yAverage = Math.round((yMin+yMax)/2);
-    // Now place the barriers:
-    for (var siteIndex=0; siteIndex<2*bCount; siteIndex+=2) {
-        var x = barrierList[index-1].locations[siteIndex] - xMin + Math.round(ydim/3);
-        var y = barrierList[index-1].locations[siteIndex+1] - yAverage + Math.round(ydim/2);
-        addBarrier(x, y);
-    }
-    paintCanvas();
-    barrierSelect.selectedIndex = 0;	// A choice on this menu is a one-time action, not an ongoing setting
-}
+
+
